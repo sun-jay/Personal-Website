94d4fd87d98d9c25c10be72c05da25b5c39ce861
sun-jay <sunny.jyrm@gmail.com>
2023-01-14 22:26:25 -0800

    added interstellar cover

--- components/Project.jsx
+++ components/Project.jsx
@@ -6,10 +6,10 @@ const Project = ({link, name, desc, color}) => {
   return (
     
     <div className= { color === "gray" ?  "grid place-items-center w-screen bg-gray-100"  : "grid place-items-center w-screen bg-white"  }>
-      <Zoom duration={1000}>
+      <Zoom duration={600} fraction = {0.1}>
       <div id="cols" className="max-w-[1240px] p-6 md:w-10/12 w-full items-center m-auto flex flex-col md:flex-row  ">
         <div id="text" className="md:hidden md:p-3 md:w-6/12 w-full">
-          <p className="text-center	md:text-left md:m-4 mb-4 text-xl text-red-600 font-bold ">
+          <p className= {name === "Bay Area Creative Youth Awards: Interstellar Main Theme - Hanz Zimmer -Performed by Sunny Jayaram"?"text-center	md:text-left md:m-4 mb-4 text-xl animate-charcter font-bold":"text-center	md:text-left md:m-4 mb-4 text-xl text-red-600 font-bold"}>
           {name}
           </p>
         </div>
@@ -23,7 +23,7 @@ const Project = ({link, name, desc, color}) => {
           ></iframe>
         </div>
         <div id="text" className="md:p-3 md:w-6/12 w-full">
-          <p className=" hidden md:block md:m-4 m-2 text-xl text-red-600 font-bold ">
+          <p className="hidden md:block md:m-4 m-2 text-xl  animate-charcter font-bold">
           {name}
           </p>
           <p className="text-center	md:text-left md:m-4 m-0 mt-5 text-sm md:text-md text-black	">
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,30 +13,37 @@ export default function Home() {
     <div className="">
       <Hero />
       {/* <Slider slides = {SliderData} /> */}
+      <Project
+        name="Bay Area Creative Youth Awards: Interstellar Main Theme - Hanz Zimmer -Performed by Sunny Jayaram"
+        desc="A piano cover of the main theme from the movie interstellar, with inspiration from Dorian Marko's viral Cornfield Chase cover and other renditions of the score. "
+        link="https://www.youtube.com/embed/yN2Wdw3ne8g"
+        color = "white"
+      />
       <Assemble_Proj />
       <Project
         name="Understanding e^ix = cos(x) + i*sin(x) in the context of infinite sums."
         desc="Mathematical Animations programmed using Python for the graphical visualization of Maclaurin polynomials, inspired by Grant Sanderson's 3Blue1Brown productions and animated using his python library Manim."
         link="https://www.youtube.com/embed/XrhA0JylwF8"
-        color = "white"
+        color = "gray"
+        special = "true"
       />
       <Project
         name="Fly me to the moon one man band - Piano, Vocals, Bass, Drums, and harp?"
         desc="A culmination of a majority of my musical talent and one of the first songs I ever learned on the piano."
         link="https://www.youtube.com/embed/wVnaSAV7QZU" 
-        color = "gray"
+        color = "white"
       />
       <Project
         name="Ms. Niazi's AP Test Data Visualized Using Python"
         desc={`Maximum effort is a staple of my work ethic; you get out what you put in. For example, my stats teacher gave everyone in the class a CSV and told us to "visualize this data." When I showed her my rendition, she ended up sending this and my Euler's formula video the entirety of the Math Department and all the Comp. Sci teachers at my school. Even the vice principal was impressed. Needless to say,  spending 7 straight hours of my Saturday on this was absolutely worth it.`}
         link="https://www.youtube.com/embed/e2O3DP_URO8" 
-        color = "white"
+        color = "gray"
       />
       <Project
         name="Fly me to the moon piano improv on a steinway in a SF hotel lobby"
         desc={`Just some improv on an amazing Steinway.`}
         link="https://www.youtube.com/embed/gTXDrLrzH6k" 
-        color = "gray"
+        color = "white"
       />
       
     </div>
