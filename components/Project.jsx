234b884c2499b00af1b6ebe50f002b2ff3ce8a10
sun-jay <sunny.jyrm@gmail.com>
2023-01-20 11:32:29 -0800

    fixed hanz zimmer

--- components/Project.jsx
+++ components/Project.jsx
@@ -9,7 +9,7 @@ const Project = ({link, name, desc, color}) => {
       <Zoom duration={600} fraction = {0.1}>
       <div id="cols" className="max-w-[1240px] p-6 md:w-10/12 w-full items-center m-auto flex flex-col md:flex-row  ">
         <div id="text" className="md:hidden md:p-3 md:w-6/12 w-full">
-          <p className= {name === "Bay Area Creative Youth Awards: Interstellar Main Theme - Hanz Zimmer -Performed by Sunny Jayaram"?"text-center	md:text-left md:m-4 mb-4 text-xl animate-charcter font-bold":"text-center	md:text-left md:m-4 mb-4 text-xl text-red-600 font-bold"}>
+          <p className= {name === "Bay Area Creative Youth Awards: Interstellar Main Theme - Hans Zimmer -Performed by Sunny Jayaram"?"text-center	md:text-left md:m-4 mb-4 text-xl animate-charcter font-bold":"text-center	md:text-left md:m-4 mb-4 text-xl text-red-600 font-bold"}>
           {name}
           </p>
         </div>
@@ -23,7 +23,7 @@ const Project = ({link, name, desc, color}) => {
           ></iframe>
         </div>
         <div id="text" className="md:p-3 md:w-6/12 w-full">
-          <p className= {name === "Bay Area Creative Youth Awards: Interstellar Main Theme - Hanz Zimmer -Performed by Sunny Jayaram"?
+          <p className= {name === "Bay Area Creative Youth Awards: Interstellar Main Theme - Hans Zimmer -Performed by Sunny Jayaram"?
           "hidden md:block md:m-4 m-2 text-xl animate-charcter font-bold":
           "hidden md:block md:m-4 m-2 text-xl text-red-600 font-bold"}>
           {name}
