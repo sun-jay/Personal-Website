1c00db760ff711ec26f63bb9d5c584b96d3e2bdc
sun-jay <sunny.jyrm@gmail.com>
2023-11-13 23:43:39 -0800

    try anim

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
+// Ensuring that the NextConfig type is recognized for autocompletion
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+  // Including webpack configuration function inside the nextConfig object
+  webpack: (config) => {
+    config.optimization.splitChunks.cacheGroups = {
+      common: {
+        name: "common",
+        chunks: "all",
+      },
+    };
 
-module.exports = {
-  images:{
-    domains:[
-      "images.unsplash.com"
-    ]
-  }
-}
+    // Custom webpack config goes here
+    return config;
+  },
+};
+
+// Exporting the configuration object
+module.exports = nextConfig;
